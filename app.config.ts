df035d00f67bdfe118fd327b629d4f28043cf72b
Steve Smith <scsmith@users.noreply.github.com>
2023-03-11 14:37:55 +0000

    Add the basics of EAS for builds
    We're not actually using EAS for builds yet, but this commit adds the
    basics of the configuration.

--- app.config.ts
+++ app.config.ts
@@ -19,19 +19,24 @@ const config: ExpoConfig = {
   assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
+    bundleIdentifier: "com.eventedapp"
   },
   android: {
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
+    package: "com.eventedapp"
   },
   web: {
     favicon: './assets/images/favicon.png',
   },
   extra: {
     apiUrl: process.env.SUPABASE_API_URL ?? 'https://localhost:54321',
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    eas: {
+      projectId: "734a6fd2-9993-47c6-b859-8f6f74ddb3cb"
+    }
   }
 };
 
